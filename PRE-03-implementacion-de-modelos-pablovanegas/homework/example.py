88a7aaa5fa01d4ff2b13711c11a1d9de09fbf200
pablovanegas <104100454+pablovanegas@users.noreply.github.com>
2025-11-17 22:54:03 -0500

    p3

---
+++ PRE-03-implementacion-de-modelos-pablovanegas/homework/example.py
@@ -0,0 +1,29 @@
+import homework.linear_regression as linear_regression  # type: ignore
+import pandas as pd  # type: ignore
+
+
+
+def main():
+    """Main function."""
+
+    # Load the data
+    data = pd.read_csv("files/input/data.csv")
+    X = data[["x1", "x2"]].values
+    y = data["y"].values
+
+    # Fit the model
+    model = linear_regression.LinearRegression()
+    model.fit(X, y)
+
+    # Make predictions
+    predictions = model.predict(X)
+
+    for y_true, y_pred in zip(y, predictions):
+        print(f"{y_true:8.4f}, {y_pred:8.4f}")
+
+
+if __name__ == "__main__":
+    main()
+#test test
+    
+    
